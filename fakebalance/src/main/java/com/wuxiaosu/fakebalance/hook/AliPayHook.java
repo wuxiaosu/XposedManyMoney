e9ed377b339795afd131df5b977517d669b6aa5f
su <1044205037@qq.com>
2018-03-11 09:35:06 +0800

    v1.0.2
    更新支持微信6.6.5，支付宝10.1.18

--- fakebalance/src/main/java/com/wuxiaosu/fakebalance/hook/AliPayHook.java
+++ fakebalance/src/main/java/com/wuxiaosu/fakebalance/hook/AliPayHook.java
@@ -1,6 +1,5 @@
 package com.wuxiaosu.fakebalance.hook;
 
-import android.util.Log;
 
 import com.wuxiaosu.fakebalance.BuildConfig;
 import com.wuxiaosu.fakebalance.util.NumberUtils;
@@ -44,6 +43,8 @@ public class AliPayHook {
             case "10.1.12":
                 break;
             case "10.1.15":
+                break;
+            case "10.1.18":
                 break;
         }
     }
